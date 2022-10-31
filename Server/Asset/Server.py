a65665fb588b017de6bbb8d5fdbfeb593a6e74c3
Vito974 <36737563+Vito974@users.noreply.github.com>
2022-10-31 20:25:17 +0400

    Update Server.py

--- Server/Asset/Server.py
+++ Server/Asset/Server.py
@@ -1,3 +1,5 @@
+#<--PANCHOO-Ashvin--PATEL-Ibrahim--DAMDJI-Imran-->
+
 import json
 import socket
 import pickle
@@ -6,7 +8,7 @@ import select
 from Classe import *
 # Create a stream based socket(i.e, a TCP socket)
 # operating on IPv4 addressing scheme
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM); 
 
 # Bind and listen
 serverSocket.bind(("",8080));
@@ -16,30 +18,30 @@ serverSocket.listen();
 while(True):
     (clientConnected, clientAddress) = serverSocket.accept();
     print("Accepted a connection request from %s:%s"%(clientAddress[0], clientAddress[1]));
-    serpent = Serpent()
-    pomme = Pomme(serpent)
+    serpent = Serpent() #Création du serpent
+    pomme = Pomme(serpent) #Création de la pomme
     
-    position = pomme.set_position()
-    p_position = json.dumps(position)
+    position = pomme.set_position() #On place la pomme
+    p_position = json.dumps(position) #Sérialisation des données pour l'envoie
     
-    clientConnected.send(p_position.encode())
+    clientConnected.send(p_position.encode()) #Envoie des données
     
     time.sleep(2)
     
-    s_direction = "U"
+    s_direction = "U" #Positionnement d'une direction par defaut
     req = 0
     while True :
-        ready = select.select([clientConnected], [], [], 1)
+        ready = select.select([clientConnected], [], [], 1) #Permet de rendre recv non bloquante
         if ready[0]:
-            req = clientConnected.recv(4096).decode()
+            req = clientConnected.recv(4096).decode() #Lecture de la touche appuyé par le client
             print(req)
         
-        if(req== "D"):
+        if(req== "D"): 
             s_direction = "U"
-            if(serpent.move("U",position)=="touché"):
-                position = pomme.set_position()
-                p_position = json.dumps(position)
-                clientConnected.send(p_position.encode())
+            if(serpent.move("U",position)=="touché"): #On verifie si on atouché la pomme
+                position = pomme.set_position() #On replace la pomme
+                p_position = json.dumps(position) 
+                clientConnected.send(p_position.encode()) #On envoie la nouvelle position de la pomme au serveur
             s_position = json.dumps(serpent.get_position())
             clientConnected.send(s_position.encode())
 
